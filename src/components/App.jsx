244b19e38ea2edf4927fcb4a0744380cf66de5d4
Ali Shah <s.ashah@outlook.com>
2021-12-30 18:33:28 -0600

    [Added] Roboto font

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,7 @@
-import { useState } from 'react';
-import logo from '../assets/logo.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count + 1)}>
-            count is:
-            {' '}
-            {count}
-          </button>
-        </p>
-        <p>
-          Edit
-          {' '}
-          <code>App.jsx</code>
-          {' '}
-          and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App min-h-screen flex flex-col bg-gradient-to-r from-blue-500 to-zinc-200">
+      <h1 className="m-auto text-3xl">Salaam :)</h1>
     </div>
   );
 }
