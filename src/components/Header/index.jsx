e9f170246e0f7531c20985641226deb14e702c8b
Ali Shah <s.ashah@outlook.com>
2022-01-14 03:35:12 -0500

    [Added] Header component
    NOTES:
    - Ported from https://github.com/shah-a/few2.3-02-weather-api-frontend/

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header className="Header text-right p-10 bg-gradient-reverse">
+      <a
+        href="https://github.com/shah-a/int2.2-pokemon-type-matchup/"
+        target="_blank"
+        rel="noreferrer"
+        className="inline-block text-teal-900 hover:text-teal-700 transition-colors motion-safe:hover:animate-wiggle"
+      >
+        <i className="fab fa-github-alt fa-3x" />
+      </a>
+    </header>
+  );
+}
+
+export default Header;
