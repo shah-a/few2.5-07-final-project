5265a44d960cf0a3f49e634eb4434b61365bd2c6
Ali Shah <s.ashah@outlook.com>
2022-01-14 15:31:03 -0500

    [Update] bars are displaying
    NOTES:
    - The lengths don't seem to be lining up perfectly along the axis,
      but it's starting to come together.

--- src/components/Display/index.jsx
+++ src/components/Display/index.jsx
@@ -7,25 +7,61 @@ function Display(props) {
   } = props;
 
   const ref = useRef();
-
+  const width = 475;
+  const height = 475;
+  const margin = 50;
   const row = data.filter((d) => d.Name === name)[0];
 
   useEffect(() => {
-    const div = d3.select(ref.current);
+    const svg = d3.select(ref.current);
 
-    div.selectAll('*').remove();
+    svg.selectAll('*').remove();
 
-    div.selectAll('p')
+    const xScale = d3.scaleLinear()
+      .domain([0, 4])
+      .range([margin, width - margin])
+      .nice();
+
+    const yScale = d3.scaleBand()
+      .domain(types)
+      .rangeRound([margin, height - margin])
+      .padding(0.125);
+
+    const colourScale = d3.scaleOrdinal()
+      .domain([0, 0.25, 0.5, 1, 2, 4])
+      .range(d3.schemeSpectral[5].reverse());
+
+    const xAxis = d3.axisBottom(xScale)
+      .tickFormat(d3.format('.2f'))
+      .ticks(5)
+      .tickValues([0, 0.25, 0.5, 1, 2, 4]);
+
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+      .attr('transform', `translate(0, ${height - margin})`)
+      .call(xAxis);
+
+    svg.append('g')
+      .attr('transform', `translate(${margin}, 0)`)
+      .call(yAxis);
+
+    svg.append('g')
+      .selectAll('rect')
       .data(types)
       .enter()
-      .append('p')
-      .text((d) => `${d}: ${row[d]}`);
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', margin + 1)
+      .attr('y', (d) => yScale(d))
+      .attr('width', (d) => xScale(row[d]))
+      .attr('height', yScale.bandwidth())
+      .attr('fill', (d) => colourScale(row[d]));
   }, [name, num]);
 
   return (
     <div className="Display flex flex-col justify-center">
-      {/* <svg width={475} height={475} ref={ref} /> */}
-      <div ref={ref} />
+      <svg width={475} height={475} ref={ref} />
     </div>
   );
 }
